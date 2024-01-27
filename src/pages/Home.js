2d3a5b75e8af73c74f217cf7e28ffdc433ffca27
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2024-01-27 17:05:57 +0100

    Added data fetching

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,47 @@
-import supabase from "../config/supabaseClient"
+import { useState, useEffect } from 'react'
+import supabase from '../config/supabaseClient'
+
+import SmoothieCard from '../components/SmoothieCard'
 
 const Home = () => {
-  console.info(supabase)
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from('smoothies')
+        .select()
+
+      if (error) {
+        setFetchError('Could not fetch smoothies')
+        setSmoothies(null)
+        console.log(error)
+      }
+
+      if (data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchSmoothies()
+  }, [])
 
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && (
+        <p>{fetchError}</p>
+      )}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
